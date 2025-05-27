4eac1aa5bde04f95cd865622723f7234864c8e7f
linhddhe173104 <dinhduclinh6823@gmail.com>
2025-05-27 20:48:19 +0700

    add taiwwincss

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./Store/store.js";
-import App from './App.jsx'
-import './Service/locales/i18n.jsx'
-
+import App from "./App.jsx";
+import "./Service/locales/i18n.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
